b190c37cdbcc4a87b4512116c28af94f0a7343d7
katra <katra89@ukr.net>
2015-09-02 16:39:06 +0300

    ..

--- js/common.js
+++ js/common.js
@@ -2,6 +2,31 @@
  * Created by Ekaterina.Porkhun on 01.09.2015.
  */
 $(function(){
+    var scrollTopBut = function(el, stopEl){
+
+        var stopElement = $(stopEl);
+        var stopPoint = stopElement.offset().top;
+        var scrolllTopValue = $(window).scrollTop(), buttonHeight = 86;
+        if(scrolllTopValue > 50) {
+            console.log('dd');
+            $(el).addClass('visible');
+            if(scrolllTopValue + $(window).height()> stopPoint) {
+                $(el).css({position: 'absolute', top: stopPoint - buttonHeight + 'px', bottom: 'auto'});
+            }
+            else {
+                $(el).css({position: 'fixed', top: 'auto', bottom: '10px'});
+            }
+        }
+        else {
+            $(el).removeClass('visible');
+        }
+    };
+    $(window).scroll(function(){
+        scrollTopBut('.button-top', '.footer');
+    });
+    $('.button-top').click(function(){
+        $('body, html').animate({scrollTop: 0}, 500);
+    });
     function closeLoginPopup(){
         $('body').removeClass('nonactive');
         $('.login-layer').removeClass('active');
